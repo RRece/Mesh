be9f9f0b44f0c052e95689a0168860167431d741
RRece <RRece@web.de>
2012-08-06 17:14:21 +0200

    Script added

---
+++ Assets/Scripts/Mesh.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class Mesh : MonoBehaviour 
+{
+	
+	 public Vector3[] newVertices;
+	
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
