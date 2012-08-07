aa5a3ff567f60d28b40c22d2c76bb0a05ef36b1d
RRece <RRece@web.de>
2012-08-07 12:12:37 +0200

    Read mesh vertices

--- Assets/Scripts/Mesh.cs
+++ Assets/Scripts/Mesh.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class Mesh : MonoBehaviour 
 {
 	
-	 public Vector3[] newVertices;
+	public Vector3[] newVertices;
 	
-	
-	
+	public UnityEngine.Mesh mesh;
+		
 	// Use this for initialization
 	void Start () 
 	{
-	
+		//UnityEngine.Mesh mesh = new UnityEngine.Mesh();
+		//GetComponent<MeshFilter>().mesh = mesh;
+		mesh = GetComponent<MeshFilter>().mesh;
+		
+		newVertices = mesh.vertices;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		mesh.vertices = newVertices;
 	}
 }
